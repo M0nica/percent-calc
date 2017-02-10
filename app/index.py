626c489ed6716a42b9deb0dcb3942fe33d4729af
Monica Powell <mmp2181@columbia.edu>
2017-02-09 23:43:24 -0500

    renamed files to follow standard Flask structure

---
+++ app/index.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+@app.route("/")
+def hello():
+    return render_template('entry.html')
+
+@app.route("/entry", methods=['POST'])
+def calculate():
+    original_number = request.form['original']
+    new_number = request.form['new']
+    result = ((int(new_number) - int(original_number))/int(original_number)) * 100
+    response =  "The percent change between " + original_number + " and " + new_number + " is " + str("%.0f%%" % result)
+
+
+    return render_template('result.html',
+                           response = response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
