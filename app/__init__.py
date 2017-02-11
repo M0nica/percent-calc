2571a438c9ea043079db4e3cfe1192a5879ae464
Monica Powell <monica@jopwell.com>
2017-02-11 09:12:16 -0500

    switched back to float

--- app/__init__.py
+++ app/__init__.py
@@ -18,10 +18,10 @@ def calculate():
     original_number = int(request.form['original'])
     new_number = int(request.form['new'])
     #result = ((int(new_number) - int(original_number))/int(original_number)) * 100
-    result = ((new_number - original_number)/int(original_number)) * 100
+    result = ((new_number - original_number)/int(original_number))
     #result = ((new_number - original_number)/int(original_number))
-    response =  "The percent change between " + str(original_number) + " and " + str(new_number) + " is " + str("%.0f%%" % result)
-
+    #response =  "The percent change between " + str(original_number) + " and " + str(new_number) + " is " + str("%.0f%%" % result)
+    response =  "The percent change between " + str(original_number) + " and " + str(new_number) + " is " + str("{:.2%}".format(result))
     #response =  "The percent change between " + str(original_number) + " and " + str(new_number) + " is " + str('{:.0%}'.format(result))
 
     return render_template('result.html',
